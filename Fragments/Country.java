33fc755e3ed547fae993faecaf7713a61c431162
umeshms66 <42529569+umeshms66@users.noreply.github.com>
2018-11-10 17:02:13 +0530

    Create Country.java

---
+++ Fragments/Country.java
@@ -0,0 +1,12 @@
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.demo.R;
+
+public class Country extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedState){
+        super.onCreate(savedState);
+        setContentView(R.layout.country);
+    }
+}
